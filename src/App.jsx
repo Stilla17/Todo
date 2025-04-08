ece59940019dc5c4d606723def4dffb3d3ae5696
Stilla <sunnatilla95@gmail.com>
2025-04-08 10:23:13 +0500

    update settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import { Route, Router, Routes } from 'react-router'
 import Dashboard from './Pages/Dashboard'
 import Plans from './Pages/Plans'
-import Calendar from './Pages/Calendar'
+import Settings from './Pages/Settings'
+import Movies from './Pages/Movies'
 import Layout from './Pages/Layout'
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path='plans' element={<Plans />} />
-          <Route path='calendar' element={<Calendar />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='movies' element={<Movies />} />
+
         </Route>
 
       </Routes>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import user_img from "./../../assets/img/user_img.png";
 import Button from "../Button/Button";
 import { FaFlagCheckered, FaEnvelope, FaBook, FaCalendarAlt, FaSignOutAlt } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
 import { Link } from "react-router";
+
+const link = [
+  {
+    link: "Todo",
+    path: "plans",
+    icon: <FaFlagCheckered />
+  },
+  {
+    link: "Movies",
+    path: "movies",
+    icon: <FaBook />
+  },
+  {
+    link: "Settings",
+    path: "settings",
+    icon: <IoMdSettings />
+  },
+
+]
+
 
 const Nav = () => {
   return (
@@ -16,30 +37,19 @@ const Nav = () => {
 
         <div className="bg-[#D9D9D9] h-[330px] rounded-b-2xl rounded-tr-[100px] flex flex-col p-6 mb-6">
           <ul className="flex flex-col gap-4">
-            <li className="flex items-center gap-2">
-              <FaFlagCheckered />
-              <Link to='plans'>Plans</Link>
-            </li>
-            <li className="flex items-center gap-2">
-              <FaFlagCheckered />
-              <a href="#">Sport</a>
-            </li>
-            <li className="flex items-center gap-2">
-              <FaEnvelope />
-              <a href="#">Mail</a>
-            </li>
-            <li className="flex items-center gap-2">
-              <FaBook />
-              <a href="#">Table Books</a>
-            </li>
-            <li className="flex items-center gap-2">
-              <FaCalendarAlt />
-              <Link to='calendar'>Calendar</Link>
-            </li>
+            {
+              link.map((item, index) => (
+                <li className="flex items-center gap-2">
+                  {item.icon}
+                  <Link to={item.path}>{item.link}</Link>
+                </li>
+              ))
+            }
+
           </ul>
 
           <div className="mt-auto flex items-center justify-end gap-4 text-gray-700">
-            <a href="#">Log out</a>
+            <Link>Log Out</Link>
             <FaSignOutAlt />
           </div>
         </div>
