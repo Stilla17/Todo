b2dbf206be2970be9b306975b0acb05da3219fdc
GigaUm001 <umargep1@gmail.com>
2025-04-12 10:16:36 +0500

    d

--- src/Pages/Cars.jsx
+++ src/Pages/Cars.jsx
@@ -1,36 +1,13 @@
 import React, { useState } from 'react'
-import { useForm } from "react-hook-form"
 
 const Cars = () => {
-
-    const [text, setText] = useState('')
-
-    const {
-        register,
-        handleSubmit,
-        watch,
-        reset,
-        formState: { errors },
-    } = useForm()
-
-    const onSubmit = (data) => {
-        console.log(data)
-
-        setText(data.example)
-        reset('')
-    }
-
-    // const inputValue = watch("example", "")
 
     return (
         <div>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input  {...register("example", { required: true })} className='border-2' />
 
-                <button className='border-2'>Submit</button>
-            </form>
-            <h1>{text}</h1>
+
+
         </div>
     )
 }
