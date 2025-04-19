57527ca4951e7f388142750d958962cd3c3b17bd
GigaUm001 <umargep1@gmail.com>
2025-04-19 10:41:47 +0500

    sss

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import MoviesCard from './../Components/MoviesCard/MoviesCard.jsx';
+import PaginationRounded from '../Components/Pagination/Pagination.jsx';
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
@@ -42,33 +43,11 @@ const Movies = () => {
       </div>
 
       
-      <div className="flex justify-center items-center gap-4 mt-8">
-        <button
-          onClick={handlePrevPage}
-          disabled={page === 1}
-          className={`px-4 py-2 rounded-lg font-medium transition 
-                ${page === 1
-              ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
-              : 'bg-primary text-white hover:bg-primary/80'}`}
-        >
-          ← Prev
-        </button>
-
-        <span className="text-base font-semibold text-neutral">
-          Page <span className="text-accent">{page}</span> / {totalPages}
-        </span>
-
-        <button
-          onClick={handleNextPage}
-          disabled={page === totalPages}
-          className={`px-4 py-2 rounded-lg font-medium transition 
-                ${page === totalPages
-              ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
-              : 'bg-primary text-white hover:bg-primary/80'}`}
-        >
-          Next →
-        </button>
-      </div>
+      <PaginationRounded
+        page={page}
+        count={totalPages}
+        onChange={(value) => setPage(value)}
+      />
 
     </div>
   );
