6f2297c7f9b1d390f009e3442353c1094471bb50
oktab <aslbekxayrullayev1@gmail.com>
2025-04-21 19:44:00 +0300

    jonatildi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,9 +8,8 @@ const Header = () => {
       <div className='flex items-center justify-between'>
         <h1 className='text-4xl'>Good morning, <span className='text-[#D9D9D9]'>Melissa</span></h1>
         <input type="text"
-          placeholder='Search'
+          placeholder='Search'  
           className='px-[20px] rounded py-[10px] bg-primary' />
-          {/* <ThemeToggle/> */}
       </div>
       <hr className='mt-10 opacity-5' />
     </header>
