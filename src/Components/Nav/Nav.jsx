7a3dfc1a3f4fa2e7551e0626fe4d3a5d04fa19cc
oktab <aslbekxayrullayev1@gmail.com>
2025-04-26 07:39:04 +0300

    yana jonatdim

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import user_img from "./../../assets/img/user_img.png";
 import Button from "../Button/Button";
@@ -83,16 +82,3 @@ const Nav = () => {
 };
 
 export default Nav;
-=======
-import React from 'react'
-
-const Nav = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
-
-export default Nav
->>>>>>> 78880e97ed1a176ee3c7bf5fbe04c921be53d5bf
