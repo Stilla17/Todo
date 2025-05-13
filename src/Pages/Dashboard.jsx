8699ab76b550ca3e2ab7327c93ca872a7f40ca07
Stilla <sunnatilla95@gmail.com>
2025-05-13 11:13:41 +0500

    fireStore qoshildi

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Dashboard = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const querySnapshot = await getDocs(collection(db, 'users'));
+            const usersData = querySnapshot.docs.map(doc => doc.data());
+            setUsers(usersData);
+        };
+        console.log(users);
+
+        fetchUsers();
+    }, []);
+
     return (
         <>
-
+            {users.map((user, idx) => (
+                <div key={idx} style={{ marginBottom: '1rem' }}>
+                    {user.photoURL && <img src={user.photoURL} width="80" alt="avatar" />}
+                    <p>{user.displayName || user.email}</p>
+                </div>
+            ))}
         </>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyBhZgWIwznZ3uRgK-NVsVEnRY42OI3OGss",
     authDomain: "movies-73b1e.firebaseapp.com",
     projectId: "movies-73b1e",
-    storageBucket: "movies-73b1e.firebasestorage.app",
+    storageBucket: "movies-73b1e.appspot.com",
     messagingSenderId: "667594968944",
     appId: "1:667594968944:web:abe35ff4daa20ba54149b5",
     measurementId: "G-0RCWJBV70C"
 };
 
+
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
+export const db = getFirestore(app);
 export { auth }
