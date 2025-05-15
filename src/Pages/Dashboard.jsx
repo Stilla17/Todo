5a018bef70ac780bb8be1a794468f2524f0ee0ce
oktab <aslbekxayrullayev1@gmail.com>
2025-05-15 08:06:38 +0300

    wefbhb

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -22,7 +22,7 @@ const Dashboard = () => {
             {/* LEFT ARROW */}
             <button
                 onClick={prevSlide}
-                className="mt-[270px] mr-[-20px] z-10 bg-white p-4 rounded-full shadow-lg hover:scale-110 transition"
+                className="mt-[270px] mr-[-30px] z-10 bg-white p-4 rounded-full shadow-lg hover:scale-110 transition"
             >
                 <svg
                     className="w-7 h-7 text-gray-700"
@@ -47,7 +47,7 @@ const Dashboard = () => {
             {/* RIGHT ARROW */}
             <button
                 onClick={nextSlide}
-                className="mt-[270px] ml-[-50px] z-10 bg-white p-4 rounded-full shadow-lg hover:scale-110 transition"
+                className="mt-[270px] ml-[-30px] z-10 bg-white p-4 rounded-full shadow-lg hover:scale-110 transition"
             >
                 <svg
                     className="w-7 h-7 text-gray-700"
@@ -59,32 +59,6 @@ const Dashboard = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
                 </svg>
             </button>
-
-            <div className="w-[500px] h-[600px] bg-white rounded-xl shadow-xl p-6">
-                <div className="flex justify-between border-b pb-4 mb-5">
-                    <button className="text-blue-600 font-semibold">Primary</button>
-                    <button className="text-gray-500 font-semibold">General</button>
-                    <button className="text-gray-500 font-semibold">Requests</button>
-                </div>
-                <ul className="space-y-5">
-                    {[
-                        'Roger Korsgaard',
-                        'Terry Torff',
-                        'Angel Bergson',
-                        'Emerson Gouse',
-                        'Corey Baptista',
-                        'Zain Culhane',
-                        'Randy Lipshutz',
-                        'Craig Botosh',
-                    ].map((name, idx) => (
-                        <li key={idx} className="flex items-center space-x-4">
-                            <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-                            <p className="text-[16px] font-medium">{name}</p>
-                        </li>
-                    ))}
-                </ul>
-                <div className="mt-6 text-blue-500 text-sm cursor-pointer hover:underline">View All</div>
-            </div>
         </div>
     );
 };
