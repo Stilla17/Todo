53e84f201d7af50c1dabbb3ccddfec52319f33ca
BKDR01 <b0327866@gmail.com>
2025-05-19 01:37:00 +0500

    Quit Chat

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -33,6 +33,11 @@ const link = [
     path: "game",
     icon: <IoGameController />
   },
+    {
+    link: "Chat",
+    path: "Chat",
+    icon: <IoGameController />
+  },
   {
     link: "Settings",
     path: "settings",
@@ -46,17 +51,37 @@ const Nav = () => {
   return (
     <nav className="shadow-2xl w-[25%] rounded-2xl h-[100vh] fixed">
       <div className="w-[90%] mx-auto">
-        <div className="flex flex-col items-center justify-center gap-4 h-[300px]">
+        <div className="flex flex-col items-center gap-4 h-[400px] bg-gray-100 mb-[20px] rounded-md w-[350px] mt-[10px]">
           {user ? (
             <>
-              <img src={user.photoURL} alt="User avatar" className="w-24 h-24 rounded-full" />
+           
+            <div className="w-[100%] h-[200px] bg-cover bg-center rounded-t-md flex items-center justify-center"  style={{ backgroundImage: `url(${user.photoURL})` }}>
+              <img
+                src={user.photoURL}
+                alt="User avatar"
+                className="w-24 h-24 rounded-full border-2 border-white mt-[60px]"
+              />
+            </div>
               <h2 className="text-2xl font-semibold">{user.displayName}</h2>
-              <Button className="text-sm bg-accent rounded-full px-4 py-2 text-white" text="Freelancer" />
+
+              <div className="bg-gray-300 w-[250px] h-[70px] px-[20px] rounded-[10px] flex justify-between items-center">
+                <div className="w-[50px] text-center"><h2 className="font-bold">100+</h2> <p className="text-gray-500">Post</p></div> 
+                <div className="border-r-1 h-[45px]"></div>
+                <div className="w-[50px] text-center"><h2 className="font-bold">200+</h2> <p className="text-gray-500">Follow</p></div> 
+                <div className="border-r-1 h-[45px]"></div>
+                <div className="w-[50px] text-center"><h2 className="font-bold">infinity+</h2> <p className="text-gray-500">Folloers</p></div>
+              </div>
+              <Button
+                className="text-sm bg-accent rounded-full px-4 py-2 text-white"
+                text="Freelancer"
+              />
             </>
           ) : (
-            <></>
+            ""
           )}
         </div>
+
+
 
         <div className="theme-preview bg-accent h-[330px] rounded-b-2xl rounded-tr-[100px] flex flex-col p-6 mb-6">
           <ul className="flex flex-col gap-4">
